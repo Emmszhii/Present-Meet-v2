3de38db771b0c911fb75c36098aaceff9c4a0f26
Emmszhii <emmanuelmanalo707@gmail.com>
2023-01-08 23:55:18 +0800

    show password

---
+++ public/javascript/register/controller.js
@@ -0,0 +1,8 @@
+import { showPassword } from './index.js';
+
+document
+  .getElementById('icon-password')
+  .addEventListener('click', showPassword);
+document
+  .getElementById('icon-password2')
+  .addEventListener('click', showPassword);
--- public/javascript/register/index.js
+++ public/javascript/register/index.js
@@ -1,3 +1,14 @@
-const showPassword = () => {};
+const showPassword = (e) => {
+  const btn = e.currentTarget;
+  const parentEl = btn.parentElement;
+  const label = parentEl.children[1];
+
+  const type = label.getAttribute('type') === 'password' ? 'text' : 'password';
+  label.setAttribute('type', type);
+  // toggle the eye slash icon
+  btn.classList.contains('fa-eye')
+    ? btn.classList.toggle('fa-eye-slash')
+    : btn.classList.toggle('fa-eye');
+};
 
 export { showPassword };
